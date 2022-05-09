815e2368f5633073fbf200cc21ae694dc2b641e7
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-09 11:35:36 -0700

    detail page fetch error

--- src/views/Detail/DetailPage.jsx
+++ src/views/Detail/DetailPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+// params (stretch: history/location)
 
 export default function DetailPage() {
-  // map over ingredients
-  // params (stretch: history/location)
+  // state: ingredients, loading, params
+  const [loading, setLoading] = useState(true);
+  const [ingredients, setIngredients] = useState([]);
+  const { id } = useParams();
+
+  // fetch: based on elixir id
+  useEffect(() => {
+    async function fetchIngredients() {
+      const res = await fetch(`https://wizard-world-api.herokuapp.com/Elixirs/${id}`);
+      const results = await res.json();
+
+      console.log(results);
+    }
+
+    fetchIngredients();
+  }, []);
+
+  // return: loading, map over ingredients
   return (
     <div>DetailPage</div>
   )
