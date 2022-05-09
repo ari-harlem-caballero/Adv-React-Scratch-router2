cbb9305a6f3bd619f8f67844e183b6e47bdfe27c
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-09 11:20:50 -0700

    list page displays

--- src/views/List/ListPage.jsx
+++ src/views/List/ListPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ListPage() {
   // list elixirs (name, effect, possible side effect), link to ingredient detail page, (stretch: color code difficulty)
@@ -19,6 +20,29 @@ export default function ListPage() {
   }, []);
 
   return (
-    <div>ListPage</div>
+    <>
+      <h1>Elixirs</h1>
+      {loading ? (
+        <figure>
+          <img
+            src='./cauldron_spinner.gif'
+            alt='green bubbling liquid in purple cauldron'/>
+          <figcaption>Loading...</figcaption>
+        </figure>
+      ) : (
+        <section>
+          {elixirs.map((elixir) => (
+            <article key={elixir.name}>
+              <h2>{elixir.name}</h2>
+              <p>{elixir.effect}</p>
+              <p>{elixir.sideEffects}</p>
+              <Link to={`ingredients/${elixir.name}`}>
+                <p>Ingredients...</p>
+              </Link>
+            </article>
+          ))}
+        </section>
+      )}
+    </>
   )
 }
